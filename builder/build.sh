7659bc0dc7c52bea977b96788e7036e53d289dfc
Get!tLive.io <admin@getitlive.io>
2014-06-29 16:27:44 +0200

    Send build raw output

--- builder/build.sh
+++ builder/build.sh
@@ -4,22 +4,27 @@ repo=$1
 branch=$2
 mode=$3
 dockerfile=$4
+taskId=$5
+auth=$6
 
 # Expand content to 'deploy' repo
 deployDir="/git/${repo}/${mode}"
 mkdir -p ${deployDir}
-GIT_WORK_TREE=${deployDir} git checkout -f ${branch}
+GIT_WORK_TREE=${deployDir} git checkout -f ${branch} || cd ${deployDir} && git checkout -f ${branch}
 cd ${deployDir}
 echo " ***** Building new Image..."
+output=""
 lastline=""
-docker -H tcp://127.0.0.1:@Model.Port build -q "${dockerfile}" | {
+docker -H tcp://127.0.0.1:5555 build -q "${dockerfile}" | {
   while IFS= read -r line
   do
     echo "$line"
+    output+="\\n${line}"
     lastline="$line"
   done
   image="`echo ${lastline} | awk '{print $3}'`"
   echo " ***** Publishing ${image} into your '${repo}' images repository..."
-  curl -s -k -XPOST -d "" -H "Token: ${APIKEY}" "http://www.getitlive.io/api/Hooks/Repository/${guid}/Done?success=true&image=${image}"
+  echo ${output} | curl -k -XPOST -d @- -u ${auth} -H 'Content-Type: application/json' -H "Token: ${APIKEY}" \
+    "http://www.getitlive.io/api/Hooks/Repository/${taskId}/Done?success=true&image=${image}"
   echo " ***** Image published!"
 }
