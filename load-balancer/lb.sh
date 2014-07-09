0b70c778f418e3253e740265b0ba10c1bec560fa
Get!tLive.io <admin@getitlive.io>
2014-07-09 19:39:11 +0200

    Add run/startup script

---
+++ load-balancer/lb.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Manage Haproxy in foreground mode 
+# Handle 'graceful' reload when container received a HUP signal
+PIDFILE="/var/run/haproxy.pid"
+haproxy="/usr/sbin/haproxy -db -f /etc/haproxy/haproxy.cfg -p ${PIDFILE}"
+
+sigquit()
+{
+   echo "signal QUIT received"
+   exit 0
+}
+
+sigint()
+{
+   echo "signal INT received, script ending"
+   exit 0
+}
+
+sighup()
+{
+  echo "HUP signal received, gracefully reload HaProxy..."
+  ${haproxy} -sf $(cat $PIDFILE)
+}
+
+trap 'sigquit' QUIT
+trap 'sigint'  INT
+trap 'sighup'  HUP
+
+# Run in foreground mode
+echo "Starting LB..."
+${haproxy}
