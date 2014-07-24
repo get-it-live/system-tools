3a1efa7ef1b69b35b53e0d0861a327a8fd44023f
Get!tLive.io <admin@getitlive.io>
2014-07-24 20:07:41 +0200

    Builder: Added use cache option

--- builder/build.sh
+++ builder/build.sh
@@ -6,6 +6,7 @@ mode=$3
 dockerfile=$4
 taskId=$5
 auth=$6
+cache=$7
 
 # Expand content to 'deploy' repo
 deployDir="/git/${repo}/${mode}"
@@ -15,10 +16,16 @@ cd ${deployDir}
 echo " ***** Building new Image..."
 output=""
 lastline=""
-docker -H tcp://127.0.0.1:5555 build -q "${dockerfile}" 2>&1 | {
+
+if [ "${cache}" == "False" ]; then
+  useCache="--no-cache"
+fi
+test -z "${useCache}" && echo " ***** Building new Image (using cache)..." || echo " ***** Building new Image (not using cache)..."
+
+docker -H tcp://127.0.0.1:5555 build -q ${useCache} "${dockerfile}" 2>&1 | {
   while IFS= read -r line
   do
-    echo "$line"
+    echo "  $line"
     output+="\\n${line}"
     lastline="$line"
   done
