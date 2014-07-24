cb0ddfe6465c3f75ddccdb178bd852a2bfd416cf
Get!tLive.io <admin@getitlive.io>
2014-07-24 16:12:56 +0200

    Fix: put back Dockerfile publishing after build.

--- builder/build.sh
+++ builder/build.sh
@@ -25,12 +25,13 @@ docker -H tcp://127.0.0.1:5555 build -q "${dockerfile}" | {
   image="`echo ${lastline} | awk '{print $3}'`"
 
   # Publish Dockerfile
-  echo ${dockerfile} | curl -k -XPOST -d @- -u ${auth} -H 'Content-Type: application/json' -H "Token: ${APIKEY}" \
+  dockerfile_path="./${dockerfile}/Dockerfile"
+  curl -k -s -XPOST --data-binary @${dockerfile_path} -u ${auth} -H 'Content-Type: text/plain' -H "Token: ${APIKEY}" \
     "http://www.getitlive.io/api/Hooks/Repository/${taskId}/Dockerfile"
 
   echo " ***** Publishing ${image} into your '${repo}' images repository..."
 
-  echo ${output} | curl -k -XPOST -d @- -u ${auth} -H 'Content-Type: application/json' -H "Token: ${APIKEY}" \
+  echo ${output} | curl -k -s -XPOST -d @- -u ${auth} -H 'Content-Type: application/json' -H "Token: ${APIKEY}" \
     "http://www.getitlive.io/api/Hooks/Repository/${taskId}/Done?success=true&image=${image}"
   echo " ***** Image published!"
 }
