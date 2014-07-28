9b3cb00b0435ab86932a0bce14014fcd3fd12221
Get!tLive.io <admin@getitlive.io>
2014-07-28 22:37:22 +0200

    LB: new Perl startup script

--- load-balancer/lb.pl
+++ load-balancer/lb.pl
@@ -3,8 +3,11 @@
 print "Starting Load Balancer\n";
 system "/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid";
  $SIG{HUP} = sub {
-    system "/usr/sbin/haproxy  -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid -sf $(cat ${PIDFILE})"; #can be "system /exitpoint"
-    die "Exiting\n"
-};
-
+    print "Reloading configuration...\n";
+    system "/usr/sbin/haproxy  -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid -sf `cat /var/run/haproxy/haproxy.pid`"; #can be "system /exitpoint"
+ };
+my $continue = 1;
+while ($continue) {
+     sleep 3;
+}
 print "Exiting launch script"
