e6ef14449ab4297f53eb05523bf63725b30c600b
Get!tLive.io <admin@getitlive.io>
2014-09-27 06:47:01 +0200

    lb: fixes

--- load-balancer/lb.pl
+++ load-balancer/lb.pl
@@ -4,7 +4,7 @@ print "Starting Load Balancer\n";
 system "/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid";
  $SIG{HUP} = sub {
     print "Reloading configuration...\n";
-    system "/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid -sf `cat /var/run/haproxy/haproxy.pid`"; #can be "system /exitpoint"
+    system "/usr/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy/haproxy.pid -sf `cat /var/run/haproxy/haproxy.pid`";
  };
 my $continue = 1;
 while ($continue) {
